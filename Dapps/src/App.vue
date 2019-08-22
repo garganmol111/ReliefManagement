5d99eba8be0d47dd3ca82e95340af8827d37f690
Iamshashvat <sha.shashvat@gmail.com>
2019-08-22 21:29:50 +0530

    Web App #layout settled static

---
+++ Dapps/src/App.vue
@@ -0,0 +1,195 @@
+<template>
+  <v-app class="grey lighten-3">
+    <v-content>
+      <v-container>
+        <v-layout
+          text-xs-center
+          wrap
+        >
+          <v-flex mb-4>
+            <h1 class="display-2 font-weight-bold mb-3">
+              Relief Funding
+            </h1>
+            <p class="subheading font-weight-regular">
+               Decentralized Peer 2 Peer Relief Fund
+            </p>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row justify-center>
+          <v-dialog v-model="startProjectDialog" max-width="600px" persistent>
+            <v-btn slot="activator" color="primary" dark>Ask for Fund</v-btn>
+            <v-card>
+              <v-card-title>
+                <span class="headline font-weight-bold mt-2 ml-4">Bring your life Again</span>
+              </v-card-title>
+              <v-card-text class="pt-0">
+                <v-container class="pt-0" grid-list-md>
+                  <v-layout wrap>
+                    <v-flex xs12>
+                      <v-text-field
+                        label="Title"
+                        persistent-hint
+                        v-model="newProject.title">
+                      </v-text-field>
+                    </v-flex>
+                    <v-flex xs12>
+                      <v-textarea
+                        label="Description"
+                        persistent-hint
+                        v-model="newProject.description">
+                      </v-textarea>
+                    </v-flex>
+                    <v-flex xs12 sm6>
+                      <v-text-field
+                        label="Amount Needed (ETH)"
+                        type="number"
+                        step="0.0001"
+                        min="0"
+                        v-model="newProject.amountGoal">
+                      </v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6>
+                      <v-text-field
+                        label="Duration (in days)"
+                        type="number"
+                        v-model="newProject.duration">
+                      </v-text-field>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  flat
+                  @click="startProjectDialog = false;
+                  newProject.isLoading = false;">
+                  Close
+                </v-btn>
+                <v-btn color="blue darken-1"
+                  flat
+                  @click="startProject"
+                  :loading="newProject.isLoading">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-layout>
+      </v-container>
+
+      <v-container
+        grid-list-lg
+      >
+        <h1 class="display-1 font-weight-bold mb-3">
+          Requests
+        </h1>
+        <v-layout row wrap>
+          <v-flex v-for="(project, index) in projectData" :key="index" xs12>
+            <v-dialog
+              v-model="project.dialog"
+              width="800"
+            >
+              <v-card>
+                <v-card-title class="headline font-weight-bold">
+                  {{ project.projectTitle }}
+                </v-card-title>
+                <v-card-text>
+                  {{ project.projectDesc }}
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="blue darken-1"
+                    flat="flat"
+                    @click="projectData[index].dialog = false"
+                  >
+                    Close
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-hover>
+              <v-card
+                slot-scope="{ hover }"
+                :class="`elevation-${hover ? 10 : 2}`"
+              >
+                <v-card-title primary-title>
+                  <div>
+                    <div class="headline font-weight-bold">
+                      <v-chip
+                        label
+                        :color="stateMap[project.currentState].color"
+                        text-color="white" class="mt-0">
+                      {{ stateMap[project.currentState].text }}
+                      </v-chip>
+                      {{ project.projectTitle }}
+                    </div>
+                    <br/>
+                    <span>{{ project.projectDesc.substring(0, 100) }}</span>
+                    <span v-if="project.projectDesc.length > 100">
+                      ... <a @click="projectData[index].dialog = true">[Show full]</a>
+                    </span>
+                    <br/><br/>
+                    <small>Up Until: <b>{{ new Date(project.deadline * 1000) }}</b></small>
+                    <br/><br/>
+                    <small>Goal of <b>{{ project.goalAmount / 10**18 }} ETH </b></small>
+                    <small v-if="project.currentState == 1">wasn't achieved before deadline</small>
+                    <small v-if="project.currentState == 2">has been achieved</small>
+                  </div>
+                </v-card-title>
+                <v-flex
+                  v-if="project.currentState == 0 && account != project.projectStarter"
+                  class="d-flex ml-3" xs12 sm6 md3>
+                  <v-text-field
+                    label="Amount (in ETH)"
+                    type="number"
+                    step="0.0001"
+                    min="0"
+                    v-model="project.fundAmount"
+                  ></v-text-field>
+                  <v-btn
+                    class="mt-3"
+                    color="light-blue darken-1 white--text"
+                    @click="fundProject(index)"
+                    :loading="project.isLoading"
+                  >
+                    Fund
+                  </v-btn>
+                </v-flex>
+                <v-flex class="d-flex ml-3" xs12 sm6 md3>
+                  <v-btn
+                    class="mt-3"
+                    color="amber darken-1 white--text"
+                    v-if="project.currentState == 1"
+                    @click="getRefund(index)"
+                    :loading="project.isLoading"
+                  >
+                    Get refund
+                  </v-btn>
+                </v-flex>
+                <v-card-actions v-if="project.currentState == 0" class="text-xs-center">
+                  <span class="font-weight-bold" style="width: 200px;">
+                    {{ project.currentAmount / 10**18 }} ETH
+                  </span>
+                  <v-progress-linear
+                    height="10"
+                    :color="stateMap[project.currentState].color"
+                    :value="(project.currentAmount / project.goalAmount) * 100"
+                  ></v-progress-linear>
+                  <span class="font-weight-bold" style="width: 200px;">
+                    {{ project.goalAmount / 10**18 }} ETH
+                  </span>
+                </v-card-actions>
+              </v-card>
+            </v-hover>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+
