d5483b39f9104f89a79c501e748356173f77f6a5
Iamshashvat <sha.shashvat@gmail.com>
2019-08-22 17:02:36 +0530

    Contract init

---
+++ Dapps/contracts/Crowdfunding.sol
@@ -0,0 +1,37 @@
+pragma solidity 0.5.4;
+import 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol';
+
+
+contract Crowdfunding {
+    using SafeMath for uint256;
+
+    Project[] private projects;
+
+    event ProjectStarted(
+        address contractAddress,
+        address projectStarter,
+        string projectTitle,
+        string projectDesc,
+        uint256 deadline,
+        uint256 goalAmount
+    );
+
+    function startProject(
+        string calldata title,
+        string calldata description,
+        uint durationInDays,
+        uint amountToRaise
+    ) external {
+        uint raiseUntil = now.add(durationInDays.mul(1 days));
+        Project newProject = new Project(msg.sender, title, description, raiseUntil, amountToRaise);
+        projects.push(newProject);
+        emit ProjectStarted(
+            address(newProject),
+            msg.sender,
+            title,
+            description,
+            raiseUntil,
+            amountToRaise
+        );
+    }                                                                                                                                   
+}
