6633d24b9de9e627e71b88e0ee4465fc3380a00d
garganmol111 <anmol.169105041@muj.manipal.edu>
2019-08-22 20:09:33 +0530

    poc dashboard request and tracking completed

--- src/app/home/alert/alert.component.ts
+++ src/app/home/alert/alert.component.ts
@@ -2,28 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-alert',
-  templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css']
+	selector: 'app-alert',
+	templateUrl: './alert.component.html',
+	styleUrls: [ './alert.component.css' ]
 })
 export class AlertComponent implements OnInit {
-  items: Observable<{}>;
-  id: String;
-  constructor(private afAuth: AngularFireAuth, private _db: AngularFireDatabase, private _router: Router) { }
+	constructor(private afAuth: AngularFireAuth, private _db: AngularFireDatabase, private _router: Router) {}
+	alerts: { 'id': string; 'location': string }[] = [];
 
-  ngOnInit() {
-    this.afAuth.authState.subscribe(res => {
-      this.id = res.uid.substring(17, res.uid.length)
-      this.items = this._db.object("users/" + res.uid).valueChanges()
-    })
-  }
+	ngOnInit() {
+		this._db.object('alert').valueChanges().subscribe((res) => {
+			console.log(Object.keys(res));
+			Object.keys(res).forEach((element) => {
+				this.alerts = [];
+				this._db.list('POC').valueChanges().subscribe((res) => {
+					res.forEach((ele) => {
+						this.alerts.push({ id: element, location: ele['location'] });
+					});
+					console.log(this.alerts);
+				});
+			});
+		});
+	}
 
-  logout() {
-    this.afAuth.auth.signOut().then(res => { this._router.navigate(['/login']) })
-  }
-
-
+	logout() {
+		this.afAuth.auth.signOut().then((res) => {
+			this._router.navigate([ '/login' ]);
+		});
+	}
 }
